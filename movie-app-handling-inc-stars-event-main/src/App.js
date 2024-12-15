9c16c0bd692851f714a33ddf2717034191392ef4
websiteonsite <skillsproject111@gmail.com>
2024-12-15 14:36:08 +0530

    first commit

---
+++ movie-app-handling-inc-stars-event-main/src/App.js
@@ -0,0 +1,11 @@
+import MovieList from "./MovieList";
+
+export default function App(){
+  return(
+    <>
+    <h1>Movie App</h1>
+    <MovieList />
+    </>
+
+  )
+}
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Moviecard from './Moviecard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movie Card</h1>
+        <Moviecard />
+    
+    </div>
+  );
+}
+
+export default App;
